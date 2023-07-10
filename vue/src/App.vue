f695e72f59ac781e01556cdea906172e87a45f7d
PARKBYEONGKEN <85853147+PARKBYEONGKEN@users.noreply.github.com>
2023-07-10 17:46:32 +0900

    0710
    0710

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
-    <div class="table_top">
-      <tbody>
-        <tr>
-          <td><router-link to="/"><img style="width:70px;" src="../src/assets/image/logo.png"></router-link></td>
-          <td>
-            <fieldset style="border:0;">
-              <legend></legend>
-              <input class="search" type="text">
-            </fieldset>
-          </td>
-        </tr>
-      </tbody>
+    <div class="table_top" style="width:100%;">
+      <div class="table_center" style="margin:0 auto; width:30%; padding-top:30px;">
+        <tbody>
+          <tr>
+            <td><router-link to="/"><img style="width:70px;" src="../src/assets/image/logo.png"></router-link></td>
+            <td>
+              <fieldset style="border:0;">
+                <legend></legend>
+                <input class="search" type="text">
+              </fieldset>
+            </td>
+            <td>서치 아이콘</td>
+          </tr>
+        </tbody>
+      </div>
+      <div>
+        <tbody>
+          <tr>
+            <td>상의</td>
+            <td>하의</td>
+            <td>아우터</td>
+            <td>신상</td>
+            <td>악세사리</td>
+          </tr>
+        </tbody>
+      </div>
     </div>
 
 
@@ -30,10 +44,4 @@ export default {
 
 <style lang="css" scoped>
 @import "../src/assets/css/menu_table.css";
-
-.search {
-  border-style: none;
-  border-bottom: solid 1px #cacaca;
-  border-collapse: collapse;
-}
 </style>
